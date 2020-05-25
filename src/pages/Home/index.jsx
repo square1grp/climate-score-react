7bd8a234b06250c223e4a66a0b4858c23119e429
square1grp <developer01@square1grp.com>
2020-05-25 12:06:09 -0700

    update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,42 +10,46 @@ import eyeDropImgSrc from '../../assets/images/eye-drop.png'
 class HomePage extends React.Component {
   render() {
     return (
-      <section className='hero-container' fluid>
-        <Carousel controls={false} indicators={false} interval={2500}>
-          {[heroOneImgSrc, heroTwoImgSrc, heroThrImgSrc].map((imgSrc) => {
-            return (
-              <Carousel.Item>
-                <Image src={imgSrc} />
-              </Carousel.Item>
-            )
-          })}
-        </Carousel>
+      <Container fluid>
+        <Row className='hero-container'>
+          <Col xs={12} className="p-0">
+            <Carousel controls={false} indicators={false} interval={2500}>
+              {[heroOneImgSrc, heroTwoImgSrc, heroThrImgSrc].map((imgSrc) => {
+                return (
+                  <Carousel.Item>
+                    <Image src={imgSrc} />
+                  </Carousel.Item>
+                )
+              })}
+            </Carousel>
 
-        <div className='hero-content d-flex'>
-          <Row className='mx-0 w-100'>
-            <Col xs={12} lg={{ span: 6, offset: 3 }}>
-              <h1 className='text-center mb-5'>How will climate change<br />affect your home?</h1>
-              <p className="text-center mb-3">Get your <b>FREE</b> Climate score</p>
+            <div className='hero-content d-flex'>
+              <Row className='mx-0 w-100'>
+                <Col xs={12} lg={{ span: 6, offset: 3 }}>
+                  <h1 className='text-center mb-5'>How will climate change<br />affect your home?</h1>
+                  <p className="text-center mb-3">Get your <b>FREE</b> Climate score</p>
 
-              <Row className="input-address-container">
-                <Col lg={{ span: 8, offset: 2 }}>
-                  <InputGroup>
-                    <InputGroup.Prepend>
-                      <InputGroup.Text id="input-address">
-                        <Image src={eyeDropImgSrc} height="20" />
-                      </InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                      placeholder="Enter an address, neighborhood, city or ZIP code"
-                      aria-describedby="input-address"
-                    />
-                  </InputGroup>
+                  <Row className="input-address-container">
+                    <Col lg={{ span: 8, offset: 2 }}>
+                      <InputGroup>
+                        <InputGroup.Prepend>
+                          <InputGroup.Text id="input-address">
+                            <Image src={eyeDropImgSrc} height="20" />
+                          </InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                          placeholder="Enter an address, neighborhood, city or ZIP code"
+                          aria-describedby="input-address"
+                        />
+                      </InputGroup>
+                    </Col>
+                  </Row>
                 </Col>
               </Row>
-            </Col>
-          </Row>
-        </div>
-      </section>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
